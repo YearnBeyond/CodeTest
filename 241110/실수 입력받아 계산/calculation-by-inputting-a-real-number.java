5d87ec5af4603dc0306fee52f7e9528c48e23c95
dongkyoung_ok <dongdong03333@gmail.com>
2024-11-10 16:33:37 +0900

    Title: 실수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241110/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
+++ 241110/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -6,6 +6,6 @@ public class Main {
         double a = sc.nextDouble();
         double b = sc.nextDouble();
         double result = a+b;
-	System.out.printf("%.2f",result)
+	    System.out.printf("%.2f",result)
     }
 }
