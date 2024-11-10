8fb77b159fa5d0c4fe77fefb78d807ed2d64439d
dongkyoung_ok <dongdong03333@gmail.com>
2024-11-10 16:36:14 +0900

    Title: 세 실수의 반올림, Time: 189ms, Memory: 11MB, Status: Passed - CodeTree

--- 241110/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ 241110/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -8,7 +8,9 @@ public class Main {
         double c = sc.nextDouble();
 
 	System.out.printf("%.3f",a);
-    System.out.printf("/n%.3f",b);
-    System.out.printf("/n%.3f",c);
+    System.out.println();
+    System.out.printf("%.3f",b);
+    System.out.println();
+    System.out.printf("%.3f",c);
     }
 }
