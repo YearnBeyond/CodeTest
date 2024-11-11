b3277d86d319bfad02aa64c2e1971506a2898a38
dongkyoung_ok <dongdong03333@gmail.com>
2024-11-11 14:44:42 +0900

    Title: 삼항연산자 2, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

--- 241111/삼항연산자 2/ternary-operator-2.java
+++ 241111/삼항연산자 2/ternary-operator-2.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println(a=1?"t":"f");
+        System.out.println(a==1?"t":"f");
         
     }
 }
