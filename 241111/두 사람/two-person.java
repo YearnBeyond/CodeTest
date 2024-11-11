1fa891ff463031c757392a04592715802b5332e1
dongkyoung_ok <dongdong03333@gmail.com>
2024-11-11 17:06:35 +0900

    Title: 두 사람, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241111/두 사람/two-person.java
+++ 241111/두 사람/two-person.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        char a1 = sc.next().charAt(0);
-        int b = sc.nextInt();
-        char b1 = sc.next().charAt(0);
-
-        if (a<19 && b<19) System.out.println("0");
-        else {
-            //if(a1=='W' && b1=='W') System.out.println("0");
-            if(a1.equals("W") && b1.equals("W")) System.out.println("0");
-            else System.out.println("1");
-        }
+		
+		int a_year = sc.nextInt();
+		char a_sex = sc.next().charAt(0); 
+		
+		int b_year  = sc.nextInt();
+		char b_sex = sc.next().charAt(0);
+		
+		if( (a_year >=19 && a_sex == 'M') || (b_year >=19 && b_sex == 'M')) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
     }
 }
