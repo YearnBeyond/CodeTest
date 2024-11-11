8492daff7c7bae9aebfb4a3266b4500da7a4da2c
dongkyoung_ok <dongdong03333@gmail.com>
2024-11-11 16:24:22 +0900

    Title: 특정 조건 세 정수 비교, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241111/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 241111/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -12,10 +12,10 @@ public class Main {
         int min = Math.min(a,b);
         min = Math.min(min,c);
 
-        if (a == min) System.out.println("1");
-        else System.out.println("0");
+        if (a == min) System.out.print("1 ");
+        else System.out.print("0 ");
 
-        if(a==b==c) System.out.println("1");
+        if(a==b && b==c) System.out.println("1");
         else System.out.println("0");
 
     }
