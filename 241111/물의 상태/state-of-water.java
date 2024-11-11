cf979c9bc28f6aa271ff6b509773daaa687efc9d
dongkyoung_ok <dongdong03333@gmail.com>
2024-11-11 14:47:20 +0900

    Title: 물의 상태, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241111/물의 상태/state-of-water.java
+++ 241111/물의 상태/state-of-water.java
@@ -6,7 +6,7 @@ public class Main {
         int a = sc.nextInt();
 
         if(a<0) System.out.println("ice");
-        else if(a>=100) System.out.println("vaper");
+        else if(a>=100) System.out.println("vapor");
         else System.out.println("water");
     }
 }
