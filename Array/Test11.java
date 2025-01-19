16c1d651567fc7007e86ab569679a9fd1519b150
dongkyoung Ok <dongdong03333@gmail.com>
2025-01-19 15:40:37 +0900

    Modified Test11.java and Test12.java

--- Array/Test11.java
+++ Array/Test11.java
@@ -3,20 +3,27 @@ package Array;
 import java.io.IOException;
 import java.util.Scanner;
 
+//********************************************************
+// 입력받은 숫자들 중 소수만 뽑아내기
+//********************************************************
+// 입력받은 숫자들 중 최대값까지의 소수 여부를 에라토스테네스의 체를 사용해 한 번에 계산
+// isNotPrime 배열을 사용하여 true는 소수가 아님, false는 소수임을 나타낸다.
+public class Test11 {
+    public boolean solution(int n){
+        if (n < 2) return false;
 
-public class Test10 {
-    public int solution(int n){
-        int answer = 0;
-
-        return answer;
+        for (int i =2; i<= Math.sqrt(n); i++) {
+            if (n % i == 0) return false; // 나눠떨어지면 소수가 아니다..
+        }
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
-		Test10 T = new Test10();
+		Test11 T = new Test11();
         Scanner sc = new Scanner(System.in);
 		
         int n = sc.nextInt();
 
-        System.out.println(T.solution(n));
+        System.out.println("소수 여부 : "+T.solution(n));
     }
 }
--- Array/Test12.java
+++ Array/Test12.java
@@ -1,22 +1,58 @@
 package Array;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+//********************************************************
+// 입력받은 숫자들 중 소수만 뽑아내기
+//********************************************************
+// 입력받은 숫자들 중 최대값까지의 소수 여부를 에라토스테네스의 체를 사용해 한 번에 계산
+// isNotPrime 배열을 사용하여 true는 소수가 아님, false는 소수임을 나타낸다.
+public class Test12 {
+    public List<Integer> solution(int num, int[] arr) {
+        // 입력받은 숫자들 중 최대값을 구한다.
+        int max = 0;
+        for (int x : arr) {
+            max = Math.max(max, x);
+        }
 
-public class Test11 {
-    public int solution(int n){
-        int answer = 0;
+        // 에라토스테네스 체를 사용해 소수 여부를 미리 계산, 저장
+        boolean[] isNotPrime = new boolean[max + 1];
+        isNotPrime[0] = true; // 0은 소수가 아님
+        isNotPrime[1] = true; // 1은 소수가 아님
+        for (int i = 2; i <= Math.sqrt(max); i++) { // 특정 숫자의 배수를 제거하기만 하면 된다.
+            if (!isNotPrime[i]) { // !소수가 아닌경우
+                for (int j = i * i; j <= max; j = i + j) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
 
-        return answer;
+        // 입력한 숫자들 중 소수만 결과로 저장하겠다.
+        // List 사용하여 동적으로 크기 저장
+        List<Integer> result = new ArrayList<>();
+        for (int x : arr) {
+            if (!isNotPrime[x]) {
+                result.add(x);
+            }
+        }
+        return result;
     }
-
     public static void main(String[] args) throws IOException {
-		Test11 T = new Test11();
+		Test12 T = new Test12();
         Scanner sc = new Scanner(System.in);
-		
-        int n = sc.nextInt();
 
-        System.out.println(T.solution(n));
+        int num = sc.nextInt();
+        int numArr[] = new int[num];
+
+        for (int i =0; i< num; i++) {
+            numArr[i] = sc.nextInt();
+        }
+
+        for (int x: T.solution(num, numArr)) {
+            System.out.print(x + " ");
+        }
     }
 }
