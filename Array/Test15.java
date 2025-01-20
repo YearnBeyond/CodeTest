c59507f8d8b81abc0075964265e7e57f6a34234f
옥동경 <dongdong03333@gmail.com>
2025-01-20 21:22:00 +0900

    봉우리

--- Array/Test15.java
+++ Array/Test15.java
@@ -8,9 +8,26 @@ import java.util.Scanner;
 // int[][]
 //********************************************************
 public class Test15 {
-    public int solution(int n, int[] arr) {
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {-1, 0, 1, 0};
+    public int solution(int n, int[][] arr) {
         int cnt = 0;
-        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k =0; k<4; k++){
+                    int nx = i+ dx[k];  // 행
+                    int ny = j+ dx[k];  // 열
+//                    if (arr[nx][ny] >= arr[i][j] && nx>=0 nx<n && ny>=0 && ny <n)
+                    if (nx>=0 && nx<n && ny>=0 && ny <n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false; // 봉우리가 아니다.
+                        break;
+                    }
+                }
+                if (flag) cnt++;
+            }
+        }
+
         return cnt;
     }
 
@@ -19,10 +36,12 @@ public class Test15 {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            for (int j=0; j<n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
         }
 
         System.out.println(T.solution(n, arr));
