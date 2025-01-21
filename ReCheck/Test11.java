0d818b56c98e29788ada489af7b2d7f1624950f2
옥동경 <dongdong03333@gmail.com>
2025-01-21 22:18:31 +0900

    commit

--- ReCheck/Test11.java
+++ ReCheck/Test11.java
@@ -5,9 +5,29 @@ import java.util.Scanner;
 
 
 public class Test11 {
-    public int solution(int num) {
-        int result = 0;
-
+    public int solution(int num, int[] arr) {
+		
+		int max = Integer.MIN_VALUE;
+		for (int x : arr) {
+			max = Math.max(x, max);
+		}
+		
+		boolean[] isNotPrime = new boolean[max + 1];
+		for (int i =2; i<=num; i++) {
+			if (!isNotPrime[i]) {
+				for (int j = i*i; j<=num; j++){
+					isNotPrime[j] = true;
+				}
+			}
+		}
+		
+		List<Integer> result = new ArrayList<>();
+		for (int x: arr) {
+			if (!isNotPrime[x]) {
+				result.add(x);
+			}
+		}
+		
 
         return result;
     }
@@ -16,7 +36,14 @@ public class Test11 {
         Test11 T = new Test11();
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+		int numArr[] = new int[num];
 
-        System.out.println(T.solution(num));
+        for (int i =0; i< num; i++) {
+            numArr[i] = sc.nextInt();
+        }
+
+        for (int x: T.solution(num, numArr)) {
+            System.out.print(x + " ");
+        }
     }
 }
