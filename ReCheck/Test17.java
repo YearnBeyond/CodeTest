6e4f4566ad0bd889d9ea4e56aa810a30b074d496
옥동경 <dongdong03333@gmail.com>
2025-02-03 21:54:08 +0900

    commit

--- ReCheck/Test17.java
+++ ReCheck/Test17.java
@@ -1,23 +1,34 @@
 package ReCheck;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Test17 {
-    public int solution(int num, int[][] arr) {
-        int answer = 0;
+    public List<Integer> solution(int n, int[] arr1, int m, int[] arr2) {
+        List<Integer> answer = new ArrayList<>();
+        
+
         return answer;
     }
 
     public static void main(String[] args) {
         Test17 T = new Test17();
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int[][] arr = new int[num][num];
-        for (int i=0; i<num; i++) {
-            for(int j=0; j<num; j++) {
-                arr[i][j] = sc.nextInt();
-            }
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        for(int i=0; i<n; i++) {
+            arr1[i] = sc.nextInt();
         }
-        System.out.println(T.solution(num, arr));
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        for(int i=0; i<m ;i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        for(int x : T.solution(n, arr1, m, arr2)) {
+            System.out.print(x+ " ");
+        }
     }
 }
