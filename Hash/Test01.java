718c2b38fa11158dbd960babd2ffc5c69a77fb69
dongkyoung <dongdong03333@gmail.com>
2025-02-06 17:23:13 +0900

    commit

---
+++ Hash/Test01.java
@@ -0,0 +1,21 @@
+package Hash;
+
+import java.util.Scanner;
+
+// 해시
+public class Test01 {
+    public String solution(int n, String str) {
+        String result ="";
+        return result;
+    }
+    public static void main(String[] args) {
+        Test01 T = new Test01();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String str = sc.next();
+
+
+
+    }
+}
